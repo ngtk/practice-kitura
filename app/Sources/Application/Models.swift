05e7ab1df10410f21456ca5a29846c46a945fe24
Kent Nagata <ngtknt@me.com>
2018-03-02 17:33:14 +0900

    Add model ToDo

---
+++ app/Sources/Application/Models.swift
@@ -0,0 +1,12 @@
+public struct ToDo : Codable, Equatable {
+    public var id: Int?
+    public var user: String?
+    public var title: String?
+    public var order: Int?
+    public var completed: Bool?
+    public var url: String?
+
+    public static func ==(lhs: ToDo, rhs: ToDo) -> Bool {
+        return (lhs.title == rhs.title) && (lhs.user == rhs.user) && (lhs.order == rhs.order) && (lhs.completed == rhs.completed) && (lhs.url == rhs.url) && (lhs.id == rhs.id)
+    }
+}
